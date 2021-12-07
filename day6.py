fcb0593a65736f67e8a8475200fb7ada3c0fed23
Shafer <ethan.shafer@westpoint.edu>
2021-12-07 09:19:34 -0500

    Day 6 solution with dict.

---
+++ day6.py
@@ -0,0 +1,42 @@
+def readFile(inputLocation):
+    with open(inputLocation, 'r') as inputFile:
+        inputLine = inputFile.readline().strip('\n')
+
+    fishDict = {}
+    for val in range(9):
+        fishDict[val] = 0
+    for val in inputLine.split(','):
+        fishDict[int(val)] += 1
+
+    return fishDict
+
+
+def stepDay(inputFish):
+    day0 = inputFish[0]
+    for val in range(8):
+        inputFish[val] = inputFish[val+1]
+    inputFish[6] += day0
+    inputFish[8] = day0
+
+
+def stepDays(inputFish, numDays):
+    for i in range(numDays):
+        stepDay(inputFish)
+
+
+inputFish = readFile('input/day6.txt')
+print(inputFish)
+stepDays(inputFish, 1)
+print(inputFish)
+stepDays(inputFish, 1)
+print(inputFish)
+stepDays(inputFish, 1)
+print(inputFish)
+stepDays(inputFish, 1)
+print(inputFish)
+stepDays(inputFish, 252)
+print(inputFish)
+fishCount = 0
+for i in range(9):
+    fishCount += inputFish[i]
+print(fishCount)
