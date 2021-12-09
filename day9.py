bd75a73258bc18ee267f903f81235b1dc37c8602
shafe123 <ethan.h.shafer@gmail.com>
2021-12-09 12:28:10 -0500

    Day 9 part 2, breadth-first search

--- day9.py
+++ day9.py
@@ -1,3 +1,5 @@
+import math
+
 def readFile(inputLocation):
     with open(inputLocation, 'r') as inputFile:
         inputLines = inputFile.readlines()
@@ -59,9 +61,52 @@ def sumRiskLevel(grid, coords):
     return result
 
 
+def aStarLimited(grid, startPoint):
+    maxX = len(grid[0])-1
+    maxY = len(grid)-1
+    toVisit = [startPoint]
+    Visited = []
+
+    while len(toVisit) > 0:
+        currentNode = toVisit.pop()
+        y = currentNode[0]
+        x = currentNode[1]
+
+        Visited.append(currentNode)
+
+        # left
+        if x != 0 and grid[y][x-1] != 9 and (y, x-1) not in Visited \
+                and (y, x-1) not in toVisit:
+            toVisit.append((y, x-1))
+        # right
+        if x != maxX and grid[y][x+1] != 9 and (y, x+1) not in Visited \
+                and (y, x+1) not in toVisit:
+            toVisit.append((y, x+1))
+        # up
+        if y != 0 and grid[y-1][x] != 9 and (y-1, x) not in Visited \
+                and (y-1, x) not in toVisit:
+            toVisit.append((y-1, x))
+        if y != maxY and grid[y+1][x] != 9 and (y+1, x) not in Visited \
+                and (y+1, x) not in toVisit:
+            toVisit.append((y+1, x))
+
+    return len(Visited)
+
+
+def findAllBasins(grid, coords):
+    basinSizes = []
+    for point in coords:
+        basinSizes.append(aStarLimited(grid, point))
+    return basinSizes
+
+
 heightMap = readFile('input/day9.txt')
 print(heightMap)
 lows = findLows(heightMap)
 print(lows)
 risk = sumRiskLevel(heightMap, lows)
 print(risk)
+basins = findAllBasins(heightMap, lows)
+basins.sort(reverse=True)
+print(basins)
+print(math.prod(basins[0:3]))
